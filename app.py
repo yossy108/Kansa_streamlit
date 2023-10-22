55cbfd91dab9e8a3b6183a11cce8499ddc8c9661
yossy108 <u.m.thsk.k108@gmail.com>
2023-10-22 13:01:40 +0900

    1st commit

--- app.py
+++ app.py
@@ -2,17 +2,8 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import pytorch_lightning as pl
-import torchmetrics
-from torchmetrics.functional import accuracy
-import torchsummary
-from torchsummary import summary
-from pytorch_lightning.loggers import CSVLogger
-import pickle
+import json
+import requests
 
 st.set_page_config(page_title="Doc Classification", layout="wide")
 st.title("csvファイルのアップロード")
@@ -34,53 +25,14 @@ else:
 
 # Predictionボタンが押された時の処理
 if st.sidebar.button("Predict"):
-
-    # モデルの構造の定義
-    class Net(nn.Module):
-
-        def __init__(self):
-            super().__init__()
-            self.fc1 = nn.Linear(1718, 200)
-            self.fc2 = nn.Linear(200, 19)
-        
-        def forward(self, x):
-            h = self.fc1(x)
-            h = F.relu(h)
-            h = self.fc2(h)
-            return h
-
-    # インスタンス化
-    net = Net().cpu().eval()
-
-    # 重みのロード（cpu環境）
-    net.load_state_dict(torch.load("kansa.pt", map_location=torch.device("cpu")))
-
-    # Vectorizerのロード
-    params = "params"
-    vectorizer = None
-    with open(params, 'rb') as f:
-        vectorizer = pickle.load(f)
-
-    # 特徴量変換（分かち書き → Vectorizer）
-    import MeCab
-    mecab = MeCab.Tagger("Owakati")
-    texts = df["監査項目"].values
-    wakati_texts = [mecab.parse(text).strip() for text in texts]
-    bow_vec = vectorizer.transform(wakati_texts).toarray()
-    bow_tensor = torch.tensor(bow_vec, dtype=torch.float32)
-
-    # 全データまとめて予測
-    pred = []
-    for i in range(bow_tensor.shape[0]):
-        x = bow_tensor[i]  # i番目の行を取得
-        with torch.no_grad():
-            y = net(x.unsqueeze(0))  # バッチサイズ1のテンソルに変換
-            y = F.softmax(y, dim=1)
-            y = torch.argmax(y)  # 最も高い確率のクラスを取得
-            pred.append(y.item())  # 予測値をpredリストに追加。.item()メソッドはTensorをスカラーに変換するメソッド
-
-    df_pred = df.copy()
-    df_pred["予測文書"] = pred
+    
+    # jsonファイルに変換
+    df_json = df.to_json(force_ascii=False,orient="split")
+    
+    # 予測の実行
+    response = requests.post("http://localhost:8000/predict", json={"dataframe_request": {"data": df_json}})
+    result_json = response.json()["result_dataframe"]
+    result_dataframe = pd.DataFrame.from_dict(json.loads(result_json), orient="split")
 
     # st.markdown("#### 予測ラベル")
     # st.table(df_pred)
@@ -95,19 +47,19 @@ if st.sidebar.button("Predict"):
         6:"監査基準書",
         7:"製品検査基準書",
         8:"識別管理基準書",
-        9:"電材機材 品質アセスメント管理基準書",
-        10:"電材機材 品質計画文書管理基準書",
-        11:"電材機材 変更管理基準書",
-        12:"電材機材 工程管理基準書",
-        13:"電材機材 検査測定機器管理基準書",
-        14:"電材機材 製造設計管理基準書",
-        15:"電材機材 設備管理基準書",
-        16:"電材機材 設計・開発管理基準書",
-        17:"電材機材 購買管理基準書",
-        18:"電材機材 顧客関連プロセス管理基準書",
+        9:"品質アセスメント管理基準書",
+        10:"品質計画文書管理基準書",
+        11:"変更管理基準書",
+        12:"工程管理基準書",
+        13:"検査測定機器管理基準書",
+        14:"製造設計管理基準書",
+        15:"設備管理基準書",
+        16:"設計・開発管理基準書",
+        17:"購買管理基準書",
+        18:"顧客関連プロセス管理基準書",
     }
 
-    df_pred_textname = df_pred.copy()
+    df_pred_textname = result_dataframe.copy()
     df_pred_textname["予測文書"] = df_pred_textname["予測文書"].replace(label_mapping)
 
     st.markdown("#### 予測文書")
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
 
 import torch
 import torch.nn as nn
@@ -15,6 +16,9 @@ from pytorch_lightning.loggers import CSVLogger
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
+
+# インスタンス化
+app = FastAPI()
 
 # モデルの構造の定義
 class Net(nn.Module):
@@ -44,24 +48,39 @@ with open(file_name, 'rb') as f:
 
 # 以上で、重み・構造・Vectorizerのロードが完了
 
-# 入力データのベクトル化
-
-# 入力データの読み込み
-df = pd.read_excel("Kansa_pred.xlsx")
-
-# 特徴量変換（分かち書き → Vectorizer）
-import MeCab
-mecab = MeCab.Tagger("Owakati")
-texts = df["監査項目"].values
-wakati_texts = [mecab.parse(text).strip() for text in texts]
-bow_vec = vectorizer.fit_transform(wakati_texts).toarray()
-bow_tensor = torch.tensor(bow_vec, dtype=torch.float32)
-
-# インスタンス化
-app = FastAPI()
+class DataFrameRequest(BaseModel):
+    data: str  # JSONデータを文字列として受け取る
 
 # トップページ
 @app.get('/')
 async def index():
     return {"Kansa": "kansa_prediction"}
 
+# POSTが送信された時のと予測値の定義
+@app.post("/predict")
+async def make_predictions(dataframe_request: DataFrameRequest):
+    json_data = json.loads(dataframe_request.data)
+    df_json_data = pd.DataFrame.from_dict(json_data, orient='split')
+    
+    # 特徴量変換（分かち書き → Vectorizer）
+    import MeCab
+    mecab = MeCab.Tagger("Owakati")
+    texts = df_json_data["監査項目"].values
+    wakati_texts = [mecab.parse(text).strip() for text in texts]
+    bow_vec = vectorizer.transform(wakati_texts).toarray()
+    bow_tensor = torch.tensor(bow_vec, dtype=torch.float32)
+
+    # 全データまとめて予測
+    pred = []
+    for i in range(bow_tensor.shape[0]):
+        x = bow_tensor[i]  # i番目の行を取得
+        with torch.no_grad():
+            y = net(x.unsqueeze(0))  # バッチサイズ1のテンソルに変換
+            y = F.softmax(y, dim=1)
+            y = torch.argmax(y)  # 最も高い確率のクラスを取得
+            pred.append(y.item())  # 予測値をpredリストに追加。.item()メソッドはTensorをスカラーに変換するメソッド
+    
+    df_pred = df_json_data.copy()
+    df_pred["予測文書"] = pred
+
+    return {"result_dataframe": df_pred.to_json(orient='split')}
